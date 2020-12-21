97c0ff7ff0467f9c817afa2c81cc79c9af0ce933
sdutwsl <wanghuilongsd@163.com>
2020-12-21 17:05:50 +0800

    修正三角位置

--- src/components/p4.vue
+++ src/components/p4.vue
@@ -3,9 +3,17 @@
     <ul>
       <li v-for="(d, i) in datas" @click="listItemClick" :key="i">
         <!-- 列表内容 -->
-        {{ d }} 
+        {{ d }}
         <!-- 悬浮框 使用if避免渲染大量dom节点-->
-        <div v-if="shows[i]" class="pop" :style="popStyle">{{ d }}</div>
+        <div
+          v-if="shows[i]"
+          class="pop"
+          id="pop"
+          :ref="'pop' + i"
+          :style="popStyle"
+        >
+          {{ d }}
+        </div>
       </li>
     </ul>
   </div>
@@ -16,26 +24,30 @@ export default {
   name: "P2",
   methods: {
     listItemClick(e) {
-        let li=e.target
-        //只允许相应li的事件
-        if(e.target.tagName==="DIV")
-            return
-        //根据InnerText判断要显示哪个悬浮框
-        this.shows=[]
-        this.shows[parseInt(li.innerText.substr(-1,1))-1]=true
-        //计算列表项的中点
-        const rect=li.getBoundingClientRect()
-        const mid=rect.top+rect.height/2
-        //根据列表项中点计算弹出框顶点和底点 若顶点<0 则贴到顶边 底点反之 如果使用的非像素高度 则使用getComputed机型实际值计算
-        const popTop=mid-75
-        const popBottom=mid+75
-        if(popTop<0){
-            this.popStyle.top=-popTop+"px"
-        }else if(popBottom>window.innerHeight){
-            this.popStyle.top=-(popBottom-window.innerHeight)+"px"
-        }else{
-            this.popStyle.top="0px"
-        }
+      let li = e.target;
+      //只允许相应li的事件
+      if (e.target.tagName === "DIV") return;
+      //根据InnerText判断要显示哪个悬浮框
+      const liIndex = parseInt(li.innerText.substr(-1, 1)) - 1;
+      this.shows = [];
+      this.shows[liIndex] = true;
+      //计算列表项的中点
+      const rect = li.getBoundingClientRect();
+      const mid = rect.top + rect.height / 2;
+      //根据列表项中点计算弹出框顶点和底点 若顶点<0 则贴到顶边 底点反之 如果使用的非像素高度 则使用getComputed机型实际值计算
+      const popTop = mid - 75;
+      const popBottom = mid + 75;
+      //顶部
+      if (popTop < 0) {
+        this.popStyle.top = -popTop + "px";
+        this.popStyle["--popTop"]="0%";
+      } else if (popBottom > window.innerHeight) {          //底部
+        this.popStyle.top = -(popBottom - window.innerHeight) + "px";
+        this.popStyle["--popTop"]="90%";
+      } else {
+        this.popStyle["--popTop"]="43%";
+        this.popStyle.top = "0px";
+      }
     },
   },
   data: function () {
@@ -51,10 +63,11 @@ export default {
         "Line8",
         "Line9",
       ],
-      shows:[],
-      popStyle:{
-          top:"0px",
-      }
+      shows: [],
+      popStyle: {
+        top: "0px",
+        '--popTop':"43%",
+      },
     };
   },
 };
@@ -90,6 +103,6 @@ ul li {
   border-color: transparent red transparent transparent;
   position: absolute;
   left: -16px;
-  top: 43%;
+  top: var(--popTop);
 }
 </style>
